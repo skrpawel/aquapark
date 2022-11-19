ff85d3bf01ac983fd3a0ac8a6a98f2682dc88bc3
Pawel <skrobskipawel@gmail.com>
2022-11-19 16:24:08 +0100

    better now

--- src/Components/Contact.tsx
+++ src/Components/Contact.tsx
@@ -5,8 +5,8 @@ import Newsletter from "./Newsletter";
 
 const Contact = () => {
   return (
-    <div className="h-1/5 bg-[#fb8500] flex items-center justify-center py-16">
-      <div className="flex flex-col md:flex-row w-1/2 h-3/4">
+    <div className="h-1/5 bg-[#fb8500] flex items-start md:items-center justify-center lg:py-16">
+      <div className="flex flex-col md:flex-row lg:w-1/2 h-3/4">
         <ContactInfo />
         <Location />
         <Newsletter />
